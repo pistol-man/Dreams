202f2b3de3dde9dbc13464690561ed891bcef41b
Varun Vyas <varun.djsce@gmail.com>
2025-07-19 03:44:08 +0530

    conversion

---
+++ dataset/csv_to_json.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+# Input and output file paths
+csv_file_path = 'Mum_predictions.csv'
+json_file_path = 'Mum_predictions.json'
+
+# Read CSV and convert to JSON
+with open(csv_file_path, mode='r', encoding='utf-8') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    data = list(csv_reader)
+
+# Write JSON output
+with open(json_file_path, mode='w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, indent=4)
+
+print(f"Converted '{csv_file_path}' to '{json_file_path}'")
